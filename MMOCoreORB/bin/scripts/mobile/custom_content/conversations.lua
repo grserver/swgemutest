0d711b2ca43c3f082f7b4aa20bac060505148a1b
lasko2112 <coll56667@yahoo.co.uk>
2015-10-31 12:49:06 +0000

    [Added] Hangar screenplay conversation

--- MMOCoreORB/bin/scripts/mobile/custom_content/conversations.lua
+++ MMOCoreORB/bin/scripts/mobile/custom_content/conversations.lua
@@ -1,3 +1,8 @@
-
-
+--Racetracks
 includeFile("custom_content/conversations/racetracks/mosrassenconversation.lua")
+
+-- Themepark Logic Conversations
+includeFile("custom_content/conversations/themepark/logic/mission_giver_conv.lua")
+includeFile("custom_content/conversations/themepark/logic/mission_target_conv.lua")
+
+
